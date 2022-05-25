a2e254a238c546229f1fa48bf0969f80eb67d165
bilalkh1 <bilal.khatab@outlook.fr>
2022-05-25 01:44:36 +0100

    feat: Working on emailing functionality with template

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const categoryRoutes = require("./routes/category-routes");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/mailing/browser')));
-
 // Body Parser
 app.use(bodyParser.urlencoded({
     extended: true
@@ -22,10 +20,5 @@ app.use(cors());
 
 app.use('/api/businesses', businessRoutes);
 app.use('/api/categories', categoryRoutes);
-
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'dist/mailing/browser', 'index.html'));
-});
-
 
 module.exports = app;
--- routes/business-routes.js
+++ routes/business-routes.js
@@ -1,59 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const Business = require('../models/Business');
+const hbs = require('nodemailer-express-handlebars');
+const path = require("path");
 
-router.post('/', (req, res) => {
-    const businesses = req.body.businesses;
-
-    Business.insertMany(businesses).then((result) => {
-        businesses.forEach((business) => {
-            sendMail(business);
-        });
-        res.json(result);
-    }).catch(err => {
-        console.log(err);
-        res.json(err);
-    })
+router.get('/', (req, res) => {
+    sendMail();
+    res.send("SENT");
 });
 
-router.get("/all", (req, res) => {
-    Business.find({})
-    .populate('category')
-    .then((businesses) => {
-        res.json(businesses);
-    }).catch((err) => {
-        console.log(err);
-        res.json(err);
-    })
-});
-
-
-
-
-function sendMail(business) {
+function sendMail() {
     let mailInfo = {
-        from: '"Grow Your Business Support" <support@growyourbusiness.tech>',
-        to: business.email,
+        from: '"Your Business Support"',
+        to: 'khatabbilal12@gmail.com',
         subject: 'Business',
-        text: `
-Bonjour Dr.${business.holderName},
-
-J'espère que vous allez bien, Je vous contacte aujourd’hui concernant le côté business pour votre cabinet.
-
-Je suis Bilal EL Khatab le propriétaire de l'agence growyourbusiness.tech, et notre travail consiste à aider les propriétaires des petites et moyennes entreprises comme vous à obtenir plus de trafic et de clients en développant des sites Web de haute qualité. Par exemple, nous venons de finaliser une mission pour elegdom.com dont l’objectif principal était de développer une application web d’achat et vente en ligne.
-
-Pour vous faire une idée de notre travail et notre portfolio, je vous invite à consulter notre site web en suivant ce lien : growyourbusiness.tech.
-
-Je vous propose de discuter de votre projet. Vous pouvez nous contacter sur notre boite mail support@growyourbusiness.tech, ou sur notre whatsapp +212689772913
-
-cordialement, 
-Bilal EL KHATAB
-`
+        template: 'email',
+        context:{
+            name: "Test", 
+            company: 'My Company'
+        }
 }
 
     let transport = nodemailer.createTransport({
-        host: 'smtp.zoho.com',
+        host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
@@ -62,13 +31,23 @@ Bilal EL KHATAB
         }
      });
 
+     // point to the template folder
+    const handlebarOptions = {
+        viewEngine: {
+            partialsDir: path.resolve('./'),
+            defaultLayout: false,
+        },
+        viewPath: path.resolve('./'),
+    };
+
+    // use a template file with nodemailer
+    transport.use('compile', hbs(handlebarOptions));
+
      transport.sendMail(mailInfo, (err, data) => {
         if (err) {
             console.log(err);
-            // res.status(501).json(err);
         }else {
             console.log(data);
-            // res.json(order);
         }
     });
 }
